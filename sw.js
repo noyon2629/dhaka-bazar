4b0944048d54dacebad11d49472fc4c6b68abfcc
noyon2629 <sordearnoyon@gmail.com>
2026-09-03 19:41:39 +0530

    Update fetch event to improve request handling
    Refactor fetch event handler to always attempt to fetch the request.

--- sw.js
+++ sw.js
@@ -12,55 +12,40 @@ const FILES = [
 self.addEventListener("install", event => {
 
     event.waitUntil(
-
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(FILES))
-
     );
 
     self.skipWaiting();
 
 });
 
-
 self.addEventListener("activate", event => {
 
     event.waitUntil(
-
         caches.keys().then(keys =>
-
             Promise.all(
-
                 keys
                     .filter(key => key !== CACHE_NAME)
                     .map(key => caches.delete(key))
-
             )
-
         )
-
     );
 
     self.clients.claim();
 
 });
 
-
 self.addEventListener("fetch", event => {
 
     event.respondWith(
-
         caches.match(event.request)
             .then(cached => {
 
                 return cached ||
-                    fetch(event.request)
-                        .catch(() =>
-                            caches.match("./index.html")
-                        );
+                    fetch(event.request);
 
             })
-
     );
 
 });
